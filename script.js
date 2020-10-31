d9ed30d2edc8299b2cc622d21429c253e947bb33
Hristo <owner@iMac.local>
2020-10-30 18:31:04 -0700

    add html content.

---
+++ script.js
@@ -0,0 +1,14 @@
+const main = document.querySelector('main');
+const voicesSelect = document.getElementById('voices');
+const textarea = document.getElementById('text');
+const readBtn = document.getElementById('read');
+const toggleBtn = document.getElementById('toggle');
+const closeBtn = document.getElementById('close');
+
+const data = [
+  {
+    image: './img/drink.jpg',
+    text: "I'm Thirsty"
+  },
+  
+]
